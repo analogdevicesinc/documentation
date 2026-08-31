analogdevicesinc/documentation

commit 4046f05474a4cc921368ef2206cc8b0ca96bf69d

.github/sphinx-build: use dirhtml
To allow no .html suffix without custom route rules.

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

.github/scripts/sphinx-build.sh
@@ -49,8 +49,8 @@ sphinx_build() {
 	fi
 
 	pushd ./docs
-	sphinx-build -b html $j_ -w "$warnfile" . _build/html --keep-going || err=$?
-	rm -rf _build/html/_sources
+	sphinx-build -b dirhtml $j_ -w "$warnfile" . _build/dirhtml --keep-going || err=$?
+	rm -rf _build/dirhtml/_sources
 	popd
 
 	# Filter false positives due to https://github.com/sphinx-doc/sphinx/pull/14325

commit 4479d23d8113d1ca54c533717179357991107270

.github/sphinx-build: still store to html
Fixes: 4046f05474a4 (".github/sphinx-build: use dirhtml")
Signed-off-by: Jorge Marques <jorge.marques@analog.com>

.github/scripts/sphinx-build.sh
@@ -49,8 +49,8 @@ sphinx_build() {
 	fi
 
 	pushd ./docs
-	sphinx-build -b dirhtml $j_ -w "$warnfile" . _build/dirhtml --keep-going || err=$?
-	rm -rf _build/dirhtml/_sources
+	sphinx-build -b dirhtml $j_ -w "$warnfile" . _build/html --keep-going || err=$?
+	rm -rf _build/html/_sources
 	popd
 
 	# Filter false positives due to https://github.com/sphinx-doc/sphinx/pull/14325